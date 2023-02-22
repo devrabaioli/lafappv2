ff7fb1527f5bc8e369bd615a36ebcfff90dc13ac
devrabaioli <devrabaioli@gmail.com>
2023-02-21 17:53:38 -0800

    First Commit

---
+++ src/main/java/dev/rabaioli/lafappv2/Lafappv2Application.java
@@ -0,0 +1,13 @@
+package dev.rabaioli.lafappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lafappv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lafappv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rabaioli/lafappv2/Lafappv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rabaioli.lafappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lafappv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
